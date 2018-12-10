8e5903e824454cb9d6c23910e86937ca6189a6a9
yuki-koike <k0001yuuki@gamil.com>
2018-12-10 05:29:35 +0000

    fix_routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root :to => 'sizes#index'
   resources :reserves, :only => [:new,:create,:index,:show,:destroy]
   resources :sizes, :only => [:show]
-  resources :users, :only => [:show, :edit,:update]
-  get 'reserves/success' => 'reserves#success'
-  get 'user/resignation' => 'users#resignnation'
+  resources :users, :only => [:show,:edit,:update]
+  get '/reserves/success' => 'reserves#success'
+  get '/user/resignation' => 'users#resignnation'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
