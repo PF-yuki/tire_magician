d397727804e449ed1802a5c93400acf367fba4b8
yuki-koike <k0001yuuki@gamil.com>
2018-12-25 08:16:27 +0000

    add_reserve

--- app/controllers/sizes_controller.rb
+++ app/controllers/sizes_controller.rb
@@ -4,17 +4,13 @@ class SizesController < ApplicationController
   end
 
   def show
-  	# manufacture#show @sizes = Size.where(name: params[:id]).order(price: :asc)
-  	@reserve = Reserve.new
-    @reserves = Reserve.all
     @size = Size.find(params[:id])
   end
 
   def select
-  	# size#index @sizes = Size.order(name: :asc).pluck(:name).uniq
   end
 
   def top
   end
-  
+
 end
